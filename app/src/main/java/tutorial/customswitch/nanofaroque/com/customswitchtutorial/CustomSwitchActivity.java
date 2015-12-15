c89709018e60f916719ff3b89163c886e52bcc6e
Polam Naganjaneyulu <polam.s.naganjaneyulu@pwc.com>
2015-12-15 23:42:46 +0530

    Initial commit

---
+++ app/src/main/java/tutorial/customswitch/nanofaroque/com/customswitchtutorial/CustomSwitchActivity.java
@@ -0,0 +1,16 @@
+package tutorial.customswitch.nanofaroque.com.customswitchtutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by pnaganjane001 on 15/12/15.
+ */
+public class CustomSwitchActivity  extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_switch);
+    }
+}
